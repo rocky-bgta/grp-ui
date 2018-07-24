bce57a010342b787093951b92a62494b0761484b
rocky-bgta <rocky.bgta@gmail.com>
2018-07-24 13:21:05 +0600

    remove un-used code and add login details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {LocationStrategy, CommonModule, HashLocationStrategy} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {CommonModule, HashLocationStrategy, LocationStrategy} from '@angular/common';
 
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
 // Routing Module
-import { AppRoutingModule } from './app.routing';
-import { FullLayoutComponent} from './layout/full-layout.component';
+import {AppRoutingModule} from './app.routing';
+import {FullLayoutComponent} from './layout/full-layout.component';
 import {Ng2AutoBreadCrumb} from "ng2-auto-breadcrumb";
-import {Ng2NewsListComponent} from "./components/ng2-newslist/ng2newslist.component";
 
 
 @NgModule({
